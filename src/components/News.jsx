c5f5fc5550b44df1844892e3df49d6a081d755f1
Cole Crutcher <colejcrutcher@gmail.com>
2020-09-08 14:11:38 -0500

    Articles displayed

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,9 +6,8 @@ export default class News extends React.Component {
     super(props);
 
     this.state = {
-      newsArray: [],
+      articlesArray: [],
     };
-    //array of news articles
   }
 
   componentDidMount() {
@@ -21,17 +20,31 @@ export default class News extends React.Component {
       'country=us&' +
       'apiKey=26f03b33e12649d497df8c7743274aad';
     let req = new Request(url);
-    fetch(req).then(function (response) {
-      console.log(response.json());
-    });
+    fetch(req)
+      .then((response) => {
+        return response.json();
+      })
+      .then((news) => {
+        this.setState({
+          articlesArray: news['articles'],
+        });
+      });
   }
 
   render() {
+    console.log(this.state);
     return (
       <div>
-        <button id='Refresh Articles' onClick={() => this.getNews()}>
-          Refresh Articles
-        </button>
+        <div>
+          <button id='Refresh Articles' onClick={() => this.getNews()}>
+            Refresh Articles
+          </button>
+        </div>
+        <div>
+          {this.state.articlesArray.map((item, index) => {
+            return <h2>{item.title}</h2>;
+          })}
+        </div>
       </div>
     );
   }
